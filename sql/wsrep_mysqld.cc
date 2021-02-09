9cf15ff3335f77031b71ca0590d5b2e9ce337f05
Daniele Sciascia <daniele.sciascia@galeracluster.com>
2021-02-09 20:06:59 +0200

    MDEV-22570 Implement wsrep_provider_options as plugin
    - Provider options are read from the provider during
      startup, before plugins are initialized.
    - New wsrep_provider plugin for which sysvars are generated
      dynamically from options read from the provider.
    - The plugin is enabled by option plugin-wsrep-provider=ON.
      If enabled, wsrep_provider_options can no longer be used,
      (an error is raised on attempts to do so).
    - Each option is either string, integer, double or bool
    - Options can be dynamic / readonly
    - Options can be deprecated
    
    Limitations:
    
    - We do not check that the value of a provider option falls
      within a certain range. This type of validation is still
      done in Galera side.
    
    Reviewed-by: Jan Lindström <jan.lindstrom@mariadb.com>

--- sql/wsrep_mysqld.cc
+++ sql/wsrep_mysqld.cc
@@ -56,7 +56,6 @@
 #include <sstream>
 
 /* wsrep-lib */
-Wsrep_server_state* Wsrep_server_state::m_instance;
 
 my_bool wsrep_emulate_bin_log= FALSE; // activating parts of binlog interface
 my_bool wsrep_preordered_opt= FALSE;
@@ -870,7 +869,8 @@ int wsrep_init()
   {
     // enable normal operation in case no provider is specified
     global_system_variables.wsrep_on= 0;
-    int err= Wsrep_server_state::instance().load_provider(wsrep_provider, wsrep_provider_options ? wsrep_provider_options : "");
+    int err= Wsrep_server_state::init_provider(
+        wsrep_provider, wsrep_provider_options ? wsrep_provider_options : "");
     if (err)
     {
       DBUG_PRINT("wsrep",("wsrep::init() failed: %d", err));
@@ -912,7 +912,7 @@ int wsrep_init()
                 "wsrep_trx_fragment_size to 0 or use wsrep_provider that "
                 "supports streaming replication.",
                 wsrep_provider, global_system_variables.wsrep_trx_fragment_size);
-    Wsrep_server_state::instance().unload_provider();
+    Wsrep_server_state::instance().deinit_provider();
     Wsrep_server_state::deinit_provider_services();
     return 1;
   }
@@ -928,7 +928,6 @@ int wsrep_init()
 
   WSREP_DEBUG("SR storage init for: %s",
               (wsrep_SR_store_type == WSREP_SR_STORE_TABLE) ? "table" : "void");
-
   return 0;
 }
 
@@ -1003,6 +1002,11 @@ void wsrep_init_startup (bool sst_first)
   wsrep_create_rollbacker();
   wsrep_create_appliers(1);
 
+  if (Wsrep_server_state::init_options())
+  {
+    WSREP_WARN("Failed to initialize provider options");
+  }
+
   Wsrep_server_state& server_state= Wsrep_server_state::instance();
   /*
     If the SST happens before server initialization, wait until the server
@@ -1028,7 +1032,7 @@ void wsrep_deinit(bool free_options)
   DBUG_ASSERT(wsrep_inited == 1);
   WSREP_DEBUG("wsrep_deinit");
 
-  Wsrep_server_state::instance().unload_provider();
+  Wsrep_server_state::deinit_provider();
   Wsrep_server_state::deinit_provider_services();
 
   provider_name[0]=    '\0';
