02efa5ba5e1364e9cf12b66d5dd0cda18c2c13d0
Oleg Smirnov <olernov@gmail.com>
2022-11-26 23:05:29 +0700

    MDEV-29693 ANALYZE TABLE still flushes table definition cache when engine-independent statistics is used
    Part 5. Add unit tests for Shared_ptr class

--- include/memory_helpers.h
+++ include/memory_helpers.h
@@ -15,11 +15,110 @@
 
 #pragma once
 
-// OLEGS: implement cross-platform
-#define MY_MT_INCR(x)                                                           \
-  _InterlockedIncrement(reinterpret_cast<volatile long *>(&x))
-#define MY_MT_DECR(x)                                                           \
-  _InterlockedDecrement(reinterpret_cast<volatile long *>(&x))
+#include <utility>
+#include "my_global.h"
+#include "my_atomic.h"
+
+template <typename Ty> class Ptr_base;
+template <typename Ty> class Reference_counter;
+
+/*
+  Shared_ptr is a smart pointer that retains shared ownership of an object
+  through a pointer. Several Shared_ptr objects may own the same object.
+  The object is destroyed and its memory deallocated when either
+  of the following happens:
+    - the last remaining Shared_ptr owning the object is destroyed;
+    - the last remaining Shared_ptr owning the object is assigned
+      another pointer via operator= or reset().
+
+  All member functions (including copy constructor and copy assignment) can be
+  called by multiple threads on different instances of Shared_ptr without
+  additional synchronization even if these instances are copies and share
+  ownership of the same object.
+
+  This implementation is inspired by the STL's std::shared_ptr
+  and has similar (though less powerful) interface.
+*/
+template <typename Type> class Shared_ptr : public Ptr_base<Type>
+{
+public:
+  Shared_ptr() noexcept= default;
+
+  // Construct empty shared_ptr
+  Shared_ptr(nullptr_t) noexcept {}
+
+  // Construct Shared_ptr object that owns object obj
+  explicit Shared_ptr(Type *obj)
+  {
+    this->managed_obj= obj;
+    this->ref_counter= new Reference_counter<Type>(obj);
+  }
+
+  // Construct Shared_ptr object that owns same resource as other
+  Shared_ptr(const Shared_ptr &other) noexcept
+  {
+    this->copy_construct_from(other);
+  }
+
+  // Construct Shared_ptr object that takes resource from right
+  Shared_ptr(Shared_ptr &&right) noexcept
+  {
+    this->move_construct_from(std::move(right));
+  }
+
+  ~Shared_ptr() noexcept { this->dec_ref_count(); }
+
+  Shared_ptr &operator=(const Shared_ptr &right) noexcept
+  {
+    Shared_ptr(right).swap(*this);
+    return *this;
+  }
+
+  template <class Type2>
+  Shared_ptr &operator=(const Shared_ptr<Type2> &right) noexcept
+  {
+    Shared_ptr(right).swap(*this);
+    return *this;
+  }
+
+  Shared_ptr &operator=(Shared_ptr &&right) noexcept
+  {
+    Shared_ptr(std::move(right)).swap(*this);
+    return *this;
+  }
+
+  template <class Type2> Shared_ptr &operator=(Shared_ptr<Type2> &&right) noexcept
+  {
+    Shared_ptr(std::move(right)).swap(*this);
+    return *this;
+  }
+
+  void swap(Shared_ptr &other) noexcept { Ptr_base<Type>::swap(other); }
+
+  // Release resource and convert to empty shared_ptr object
+  void reset() noexcept { Shared_ptr().swap(*this); }
+
+  // Release, take ownership of obj
+  template <typename Type2> void reset(Type2 *obj) noexcept
+  {
+    Shared_ptr(obj).swap(*this);
+  }
+
+  using Ptr_base<Type>::get;
+
+  template <typename Type2= Type> Type2 &operator*() const noexcept
+  {
+    return *get();
+  }
+
+  template <typename Type2= Type> Type2 *operator->() const noexcept
+  {
+    return get();
+  }
+
+  explicit operator bool() const noexcept { return get() != nullptr; }
+};
+
 
 /* Base class for reference counting */
 class Reference_counter_base
@@ -37,34 +136,35 @@ public:
   virtual ~Reference_counter_base() noexcept { }
 
   // Increment use count
-  void incref() noexcept
+  void inc_ref_count() noexcept
   {
-    MY_MT_INCR(uses);
+    my_atomic_add32(&uses, 1);
   }
 
   // Decrement use count
-  void decref() noexcept
+  void dec_ref_count() noexcept
   {
-    if (MY_MT_DECR(uses) == 0)
+    int32 prev_value= 0;
+    if ((prev_value= my_atomic_add32(&uses, -1)) == 1)
     {
       destroy();
     }
   }
 
-  unsigned long use_count() const noexcept { return uses; }
+  unsigned long use_count() noexcept { return my_atomic_load32(&uses); }
 
 private:
   virtual void destroy() noexcept= 0; /* destroy managed resource */
 
-  unsigned long uses= 1;
+  volatile int32 uses= 1;
 };
 
 
-template <typename Ty>
+template <typename Type>
 class Reference_counter : public Reference_counter_base
 { 
 public:
-  explicit Reference_counter(Ty *obj) :
+  explicit Reference_counter(Type *obj) :
     Reference_counter_base(), managed_obj(obj) 
   {}
 
@@ -75,14 +175,12 @@ private:
     delete managed_obj;
   }
 
-  Ty *managed_obj;
+  Type *managed_obj;
 };
 
 
-template <typename Ty> class Shared_ptr;
-
-// Base class for shared_ptr and weak_ptr
-template <typename Ty> class Ptr_base
+// Base class for Shared_ptr and Weak_ptr
+template <typename Type> class Ptr_base
 {
 public:
   long use_count() const noexcept
@@ -97,13 +195,13 @@ public:
   Ptr_base &operator=(Ptr_base &&)= delete;
 
 protected:
-  Ty *get() const noexcept { return managed_obj; }
+  Type *get() const noexcept { return managed_obj; }
 
   Ptr_base() noexcept= default;
 
-  // shared_ptr's (converting) move ctor
-  template <typename Ty2>
-  void move_construct_from(Ptr_base<Ty2> &&other) noexcept
+  // Shared_ptr's (converting) move ctor
+  template <typename Type2>
+  void move_construct_from(Ptr_base<Type2> &&other) noexcept
   {
     managed_obj= other.managed_obj;
     ref_counter= other.ref_counter;
@@ -113,28 +211,28 @@ protected:
   }
 
   // Shared_ptr's (converting) copy ctor
-  template <typename Ty2>
-  void copy_construct_from(const Shared_ptr<Ty2> &other) noexcept
+  template <typename Type2>
+  void copy_construct_from(const Shared_ptr<Type2> &other) noexcept
   {
-    other.incref();
+    other.inc_ref_count();
 
     managed_obj= other.managed_obj;
     ref_counter= other.ref_counter;
   }
 
-  void incref() const noexcept
+  void inc_ref_count() const noexcept
   {
     if (ref_counter)
     {
-      ref_counter->incref();
+      ref_counter->inc_ref_count();
     }
   }
 
-  void decref() noexcept
+  void dec_ref_count() noexcept
   { 
     if (ref_counter)
     {
-      ref_counter->decref();
+      ref_counter->dec_ref_count();
     }
   }
 
@@ -150,152 +248,11 @@ protected:
   }
   
 private:
-  Ty *managed_obj= nullptr;
+  Type *managed_obj= nullptr;
   Reference_counter_base *ref_counter= nullptr;
 
-  template <class Ty0> friend class Ptr_base;
+  template <class Type0> friend class Ptr_base;
 
-  friend Shared_ptr<Ty>;
+  friend Shared_ptr<Type>;
 };
 
-
-// OLEGS: prevent creation for arrays like: Shared_ptr<int> p(new int(100))
-
-// class for reference counted resource management
-template <typename Ty> class Shared_ptr : public Ptr_base<Ty>
-{ 
-public:
-  Shared_ptr() noexcept= default;
-
-  // construct empty shared_ptr
-  Shared_ptr(nullptr_t) noexcept {}
-
-  // construct Shared_ptr object that owns object obj
-  template <typename Ux> explicit Shared_ptr(Ux *obj)
-  { 
-    this->managed_obj= obj;
-    this->ref_counter= new Reference_counter<Ux>(obj);
-  }
-
-  // construct shared_ptr object that owns same resource as other
-  Shared_ptr(const Shared_ptr &other) noexcept
-  {
-    this->copy_construct_from(other);
-  }
-
-  // construct shared_ptr object that takes resource from right
-  Shared_ptr(Shared_ptr &&right) noexcept
-  {
-    this->move_construct_from(std::move(right));
-  }
-
-  ~Shared_ptr() noexcept
-  { 
-    // release resource
-    this->decref();
-  }
-
-  Shared_ptr &operator=(const Shared_ptr &right) noexcept
-  {
-    Shared_ptr(right).swap(*this);
-    return *this;
-  }
-
-  template <class Ty2>
-  Shared_ptr &operator=(const Shared_ptr<Ty2> &right) noexcept
-  {
-    Shared_ptr(right).swap(*this);
-    return *this;
-  }
-
-  Shared_ptr &operator=(Shared_ptr &&right) noexcept
-  {
-    Shared_ptr(std::move(right)).swap(*this);
-    return *this;
-  }
-
-  template <class Ty2>
-  Shared_ptr &operator=(Shared_ptr<Ty2> &&right) noexcept
-  {
-    Shared_ptr(std::move(right)).swap(*this);
-    return *this;
-  }
-
-  void swap(Shared_ptr &other) noexcept { Ptr_base<Ty>::swap(other); }
-
-  // release resource and convert to empty shared_ptr object
-  void reset() noexcept
-  {
-    Shared_ptr().swap(*this);
-  }
-
-  // release, take ownership of obj
-  template <typename Ux> void reset(Ux *obj)
-  {
-    Shared_ptr(obj).swap(*this);
-  }
-
-  using Ptr_base<Ty>::get;
-
-  template<typename Ty2= Ty>
-  Ty2 &operator*() const noexcept
-  {
-    return *get();
-  }
-
-  template <typename Ty2= Ty>
-  Ty2 *operator->() const noexcept
-  {
-    return get();
-  }
-
-  explicit operator bool() const noexcept
-  {
-    return get() != nullptr;
-  }
-
-private:
-  /* template <class _UxptrOrNullptr, class _Dx>
-  void _Setpd(const _UxptrOrNullptr _Px, _Dx _Dt)
-  { // take ownership of _Px, deleter _Dt
-    _Temporary_owner_del<_UxptrOrNullptr, _Dx> _Owner(_Px, _Dt);
-    _Set_ptr_rep_and_enable_shared(
-        _Owner._Ptr, new _Ref_count_resource<_UxptrOrNullptr, _Dx>(
-                         _Owner._Ptr, _STD move(_Dt)));
-    _Owner._Call_deleter= false;
-  }
-
-  template <class _UxptrOrNullptr, class _Dx, class _Alloc>
-  void _Setpda(const _UxptrOrNullptr _Px, _Dx _Dt, _Alloc _Ax)
-  { // take ownership of _Px, deleter _Dt, allocator _Ax
-    using _Alref_alloc= _Rebind_alloc_t<
-        _Alloc, _Ref_count_resource_alloc<_UxptrOrNullptr, _Dx, _Alloc>>;
-
-    _Temporary_owner_del<_UxptrOrNullptr, _Dx> _Owner(_Px, _Dt);
-    _Alref_alloc _Alref(_Ax);
-    _Alloc_construct_ptr<_Alref_alloc> _Constructor(_Alref);
-    _Constructor._Allocate();
-    _Construct_in_place(*_Constructor._Ptr, _Owner._Ptr, _STD move(_Dt), _Ax);
-    _Set_ptr_rep_and_enable_shared(_Owner._Ptr, _Unfancy(_Constructor._Ptr));
-    _Constructor._Ptr= nullptr;
-    _Owner._Call_deleter= false;
-  }
-
-  template <class _Ty0, class... _Types>
-  friend shared_ptr<_Ty0> make_shared(_Types &&..._Args);
-  
-  template <typename Ux>
-  void _Set_ptr_rep_and_enable_shared(Ux *const _Px,
-                                      Reference_counter_base *const _Rx) noexcept
-  { // take ownership of _Px
-    this->_Ptr= _Px;
-    this->_Rep= _Rx;
-  }
-
-  void _Set_ptr_rep_and_enable_shared(nullptr_t,
-                                      _Ref_count_base *const _Rx) noexcept
-  { // take ownership of nullptr
-    this->_Ptr= nullptr;
-    this->_Rep= _Rx;
-  }*/
-};
